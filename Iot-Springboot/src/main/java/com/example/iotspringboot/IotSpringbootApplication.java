05644b68f8abeb917afefd27a6b10795c9fb95a3
Alperen941 <alperenozer03@gmail.com>
2025-04-01 19:55:51 +0200

    created spring boot project

---
+++ Iot-Springboot/src/main/java/com/example/iotspringboot/IotSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.iotspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IotSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IotSpringbootApplication.class, args);
+	}
+
+}
---
+++ Iot-Springboot/src/test/java/com/example/iotspringboot/IotSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.iotspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IotSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
