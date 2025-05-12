63072ef812bdc2b15c28ac564a06260cc4362871
Alperen941 <alperenozer03@gmail.com>
2025-05-12 13:53:39 +0200

    deleted embedded folder

--- IoT-Service-main/Iot-Springboot/src/main/java/com/example/iotspringboot/repository/LightRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.iotspringboot.repository;
-
-import com.example.iotspringboot.model.Light;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LightRepository extends JpaRepository<Light, Long>
-{
-  Light findTopByOrderByTimeStampDesc();
-}
